e9b4f7f1e18f0245cbd2bbcf80bf238b8fac55f0
liuzhe429 <543270835@qq.com>
2017-09-25 17:31:04 +0800

    修改导航

--- app/App.js
+++ app/App.js
@@ -18,9 +18,8 @@ import DealListScreen from './containers/DealList/DealListScreen'
 import DealDetailScreen from './containers/DealDetail/DealDetailScreen'
 import SxyDetailScreen from './containers/SxyDetail/SxyDetailScreen'
 import DealConfirmScreen from './containers/DealConfirm/DealConfirmScreen'
-
 // 通过TabNavigator做路由映射
-const MainScreentNavigator = TabNavigator({
+const MainScreentNavigator=TabNavigator({
   Home:{
     screen:HomeScreen,
     navigationOptions:{
@@ -101,28 +100,29 @@ const MainScreentNavigator = TabNavigator({
 })
 //引入要用到的跳转页面
 const  MyNavigatior = StackNavigator({
-  Main:{screen:MainScreentNavigator},
-  DealList:{
-    screen:DealListScreen,
-    navigationOptions:{
-    // headerLeft:<View style={{width:60,flex:1,flexDirection:"row",justifyContent:"flex-start",alignItems:"center"}}>
-    //             <TouchableOpacity activeOpacity={1} style={{width:"100%",flexDirection:"row",alignItems:"center"}}>
-    //               <Text style={{width:10,marginRight:2,marginLeft:10,marginTop:7}}>
-    //                 <Image
-    //                   source={require('./images/back.png')}
-    //                   style={{width:9,height:17}}/>
-    //               </Text>
-    //               <Text style={{fontSize:14,color:"#41474d"}}
-    //                 onPress={() => this.props.navigator.pop()}>返回</Text>
-                  
-    //             </TouchableOpacity>
-    //           </View>
-    }
-  },
-  DealDetail:{screen:DealDetailScreen},
-  SxyDetail:{screen:SxyDetailScreen},
-  DealConfirm:{screen:DealConfirmScreen}
+    Main:{screen:MainScreentNavigator},
+    DealList:{
+      screen:DealListScreen,
+      navigationOptions:{
+      // headerLeft:<View style={{width:60,flex:1,flexDirection:"row",justifyContent:"flex-start",alignItems:"center"}}>
+      //             <TouchableOpacity activeOpacity={1} style={{width:"100%",flexDirection:"row",alignItems:"center"}}>
+      //               <Text style={{width:10,marginRight:2,marginLeft:10,marginTop:7}}>
+      //                 <Image
+      //                   source={require('./images/back.png')}
+      //                   style={{width:9,height:17}}/>
+      //               </Text>
+      //               <Text style={{fontSize:14,color:"#41474d"}}
+      //                 onPress={() => this.props.navigator.pop()}>返回</Text>
+                    
+      //             </TouchableOpacity>
+      //           </View>
+      }
+    },
+    DealDetail:{screen:DealDetailScreen},
+    SxyDetail:{screen:SxyDetailScreen},
+    DealConfirm:{screen:DealConfirmScreen}
 })
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -139,5 +139,4 @@ const styles = StyleSheet.create({
     marginRight:0,
   }
 })
-
-AppRegistry.registerComponent('wangxinnative', () => MainScreentNavigator);
+AppRegistry.registerComponent('wangxinnative', () => MyNavigatior);
